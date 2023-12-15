2891aa9eb7f17997d349a63a162b0450f2f330f6
hallan-n <hallanneves01@gmail.com>
2023-12-15 19:14:40 -0300

    Finish conecction

--- app/main.py
+++ app/main.py
@@ -1,7 +1,35 @@
 from fastapi import FastAPI
+import mysql.connector
 
 app = FastAPI()
 
+
 @app.get("/")
-def entry():
-    return {"message": "Hello World!"}
+def try_conn():
+    resp = get_conn()
+    return {"message": resp}
+
+
+def get_conn():
+    try:
+        _create_db()
+        mysql.connector.connect(
+            host="172.21.0.3",
+            user="root",
+            password="123456",
+            database="db_teste",
+        )
+        return "Conectou no banco"
+    except:
+        return "Não conectou no banco"
+
+
+def _create_db():
+    try:
+        mydb = mysql.connector.connect(
+            host="172.21.0.3", user="root", password="123456"
+        )
+        mycursor = mydb.cursor()
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS db_teste")
+    except:
+        raise Exception("Erro ao criar o banco")
